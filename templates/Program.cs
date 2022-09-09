eb6fa48459c7f2a7af841a0d4ef97cea368aac03
Igor Chichinin <marafon4k@gmail.com>
2022-09-09 12:55:07 +0300

    Added solutions

---
+++ templates/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] array = new string[] { "hello", "2", "world", ":-)" };
+string[] newArray = new string[array.Length];
+
+void CheckArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) newArray[i] = array[i];
+    }
+}
+
+CheckArray(array);
+
+void PrintArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != "") Console.Write($"{arr[i]} ");
+    }
+}
+
+PrintArr(newArray);
